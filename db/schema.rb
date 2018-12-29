42e0c2151a2706a0840862040fca8ccbd11b389c
Ambrose Hundal <amshunda@ucsc.edu>
2018-12-28 22:49:35 -0800

    added quantity column to model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181229064212) do
+ActiveRecord::Schema.define(version: 20181229064721) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "item_name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20181229064212) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "grocery_id"
+    t.integer  "quantity"
   end
 
   create_table "groceries", force: :cascade do |t|
