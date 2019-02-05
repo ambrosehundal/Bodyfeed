46aa0e80cf9f1f1108e3cefa94fd020be8ee93ff
Ambrose Hundal <amshunda@ucsc.edu>
2019-02-04 23:22:31 -0800

    testing previews of emails

--- app/mailers/food_mailer.rb
+++ app/mailers/food_mailer.rb
@@ -2,7 +2,7 @@ class FoodMailer < ApplicationMailer
    default from: "railshundal@gmail.com"
 
 
-   def test_email(foodie)
+   def sample_email(foodie)
         @foodie = foodie
         mail(to: @foodie.email, subject: 'Foods expiration dates')
    end
